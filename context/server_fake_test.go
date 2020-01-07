306b91bf0a9e2d2db30a3400b3a470fc3ab6d174
chensheng06 <chensheng06@baidu.com>
2020-01-07 12:53:28 +0800

    move server_fake to context from api

--- context/server_fake_test.go
+++ context/server_fake_test.go
@@ -1,14 +1,16 @@
-package api
+package context
 
 import (
 	"context"
-	"github.com/baetyl/baetyl-go/link"
-	"google.golang.org/grpc"
 	"net"
 	"testing"
 
+	"github.com/baetyl/baetyl-go/api"
+	"github.com/baetyl/baetyl-go/link"
 	"github.com/gogo/protobuf/types"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 )
 
 // FakeServer the fake of link server for test only
@@ -24,7 +26,7 @@ func FakeServer(t *testing.T, port string, auth link.Authenticator) *grpc.Server
 	}
 	svr := grpc.NewServer(opts...)
 
-	RegisterKVServiceServer(svr, &mockKVService{
+	api.RegisterKVServiceServer(svr, &mockKVService{
 		m: make(map[string][]byte),
 	})
 	lis, err := net.Listen("tcp", ":"+port)
@@ -39,35 +41,55 @@ type mockKVService struct {
 }
 
 // Set set kv
-func (s *mockKVService) Set(ctx context.Context, kv *KV) (*types.Empty, error) {
+func (s *mockKVService) Set(ctx context.Context, kv *api.KV) (*types.Empty, error) {
 	s.m[string(kv.Key)] = kv.Value
 	return new(types.Empty), nil
 }
 
 // Get get kv
-func (s *mockKVService) Get(ctx context.Context, kv *KV) (*KV, error) {
-	return &KV{
+func (s *mockKVService) Get(ctx context.Context, kv *api.KV) (*api.KV, error) {
+	return &api.KV{
 		Key:   kv.Key,
 		Value: s.m[string(kv.Key)],
 	}, nil
 }
 
 // Del del kv
-func (s *mockKVService) Del(ctx context.Context, kv *KV) (*types.Empty, error) {
+func (s *mockKVService) Del(ctx context.Context, kv *api.KV) (*types.Empty, error) {
 	delete(s.m, string(kv.Key))
 	return new(types.Empty), nil
 }
 
 // List list kvs with prefix
-func (s *mockKVService) List(ctx context.Context, kv *KV) (*KVs, error) {
-	kvs := KVs{
-		Kvs: []*KV{},
+func (s *mockKVService) List(ctx context.Context, kv *api.KV) (*api.KVs, error) {
+	kvs := api.KVs{
+		Kvs: []*api.KV{},
 	}
 	for k, v := range s.m {
-		kvs.Kvs = append(kvs.Kvs, &KV{
+		kvs.Kvs = append(kvs.Kvs, &api.KV{
 			Key:   []byte(k),
 			Value: v,
 		})
 	}
 	return &kvs, nil
+}
+
+type mockAuthenticator struct{}
+
+func (auth mockAuthenticator) Authenticate(ctx context.Context) error {
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return link.ErrUnauthenticated
+	}
+	var u, p string
+	if val, ok := md[link.KeyUsername]; ok {
+		u = val[0]
+	}
+	if val, ok := md[link.KeyPassword]; ok {
+		p = val[0]
+	}
+	if u != "baetyl" || p != "baetyl" {
+		return link.ErrUnauthenticated
+	}
+	return nil
 }
