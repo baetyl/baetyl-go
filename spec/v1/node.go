7102cd5068d81c5e2c8e22a07007ff7cccb2eb9a
xiejingru <xiejingru@baidu.com>
2020-05-26 17:49:45 +0800

    adjustment of getting app info (#97)

--- spec/v1/node.go
+++ spec/v1/node.go
@@ -65,29 +65,9 @@ type Report map[string]interface{}
 // Desire desire data
 type Desire map[string]interface{}
 
-// AppInfos return app infos
-func (r Report) AppInfos() []AppInfo {
-	return getAppInfos("apps", r)
-}
-
-// AppInfos return sysapps infos
-func (r Report) SysAppInfos() []AppInfo {
-	return getAppInfos("sysapps", r)
-}
-
 // Merge merge new reported data
 func (r Report) Merge(reported Report) error {
 	return merge(r, reported, 1, maxJSONLevel)
-}
-
-// AppInfos return app infos
-func (d Desire) AppInfos() []AppInfo {
-	return getAppInfos("apps", d)
-}
-
-// AppInfos return sysapps infos
-func (d Desire) SysAppInfos() []AppInfo {
-	return getAppInfos("sysapps", d)
 }
 
 // Merge merge new reported data
@@ -209,7 +189,7 @@ func (s *ServiceInfo) translateResouceQuantity() {
 
 }
 
-func getAppInfos(appType string, data map[string]interface{}) []AppInfo {
+func GetAppInfos(appType string, data map[string]interface{}) []AppInfo {
 	if data == nil {
 		return nil
 	}
