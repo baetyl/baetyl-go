d1c1d5dc4723c29b973119b1717a40e1783025a0
chensheng <chensheng06@baidu.com>
2020-09-14 19:39:20 +0800

    add native mapping related (#179)
    * add native mapping related
    
    * add mutex

---
+++ native/mapping.go
@@ -0,0 +1,83 @@
+package native
+
+import (
+	"io/ioutil"
+	"os"
+	"path"
+	"sync"
+
+	"gopkg.in/yaml.v2"
+
+	"github.com/baetyl/baetyl-go/v2/errors"
+	"github.com/baetyl/baetyl-go/v2/utils"
+)
+
+const (
+	serviceMappingFile = "var/lib/baetyl/run/services.yml"
+)
+
+type ServiceMapping struct {
+	Services map[string]ServiceMappingInfo `yaml:"services,omitempty"`
+	sync.RWMutex
+}
+
+type ServiceMappingInfo struct {
+	Ports PortsInfo `yaml:"ports,omitempty"`
+}
+
+type PortsInfo struct {
+	Items  []int `yaml:"items,omitempty"`
+	offset int
+}
+
+func (i *PortsInfo) Next() (int, error) {
+	if len(i.Items) == 0 {
+		return 0, errors.New("ports of service are empty in ports mapping file")
+	}
+	port := i.Items[i.offset]
+	i.offset++
+	i.offset = i.offset % len(i.Items)
+	return port, nil
+}
+
+func NewServiceMapping() (*ServiceMapping, error) {
+	m := &ServiceMapping{
+		Services: make(map[string]ServiceMappingInfo),
+	}
+	return m, nil
+}
+
+func (s *ServiceMapping) Load() error {
+	s.Lock()
+	defer s.Unlock()
+
+	if !utils.FileExists(serviceMappingFile) {
+		return errors.Errorf("services mapping file (%s) doesn't exist", serviceMappingFile)
+	}
+	data, err := ioutil.ReadFile(serviceMappingFile)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	err = yaml.Unmarshal(data, s)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	return nil
+}
+
+func (s *ServiceMapping) Save() error {
+	s.Lock()
+	defer s.Unlock()
+
+	data, err := yaml.Marshal(s)
+	if err != nil {
+		return errors.Trace(err)
+	}
+	if !utils.PathExists(path.Dir(serviceMappingFile)) {
+		err = os.MkdirAll(path.Dir(serviceMappingFile), 0755)
+		if err != nil {
+			return errors.Trace(err)
+		}
+	}
+	return ioutil.WriteFile(serviceMappingFile, data, 0755)
+}
