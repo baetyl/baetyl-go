dd2017ae5eef41fdf5ac65c98bec6723c8a43a14
chensheng06 <chensheng06@baidu.com>
2020-09-15 14:10:50 +0800

    add ServiceMapping struct

--- native/mapping.go
+++ native/mapping.go
@@ -6,9 +6,11 @@ import (
 	"path"
 	"sync"
 
+	"github.com/fsnotify/fsnotify"
 	"gopkg.in/yaml.v2"
 
 	"github.com/baetyl/baetyl-go/v2/errors"
+	"github.com/baetyl/baetyl-go/v2/log"
 	"github.com/baetyl/baetyl-go/v2/utils"
 )
 
@@ -32,7 +34,7 @@ type PortsInfo struct {
 
 func (i *PortsInfo) Next() (int, error) {
 	if len(i.Items) == 0 {
-		return 0, errors.New("ports of service are empty in ports mapping file")
+		return 0, errors.New("ports of service are empty in services mapping file")
 	}
 	port := i.Items[i.offset]
 	i.offset++
@@ -46,10 +48,7 @@ func NewServiceMapping() *ServiceMapping {
 	}
 }
 
-func (s *ServiceMapping) Load() error {
-	s.Lock()
-	defer s.Unlock()
-
+func (s *ServiceMapping) load() error {
 	if !utils.FileExists(ServiceMappingFile) {
 		return errors.Errorf("services mapping file (%s) doesn't exist", ServiceMappingFile)
 	}
@@ -64,10 +63,7 @@ func (s *ServiceMapping) Load() error {
 	return nil
 }
 
-func (s *ServiceMapping) Save() error {
-	s.Lock()
-	defer s.Unlock()
-
+func (s *ServiceMapping) save() error {
 	data, err := yaml.Marshal(s)
 	if err != nil {
 		return errors.Trace(err)
@@ -79,4 +75,97 @@ func (s *ServiceMapping) Save() error {
 		}
 	}
 	return ioutil.WriteFile(ServiceMappingFile, data, 0755)
+}
+
+func (s *ServiceMapping) AddServicePorts(serviceName string, ports []int) error {
+	s.Lock()
+	defer s.Unlock()
+
+	s.Services[serviceName] = ServiceMappingInfo{
+		Ports: PortsInfo{
+			Items: ports,
+		},
+	}
+	err := s.save()
+	if err != nil {
+		return errors.Trace(err)
+	}
+	return nil
+}
+
+func (s *ServiceMapping) DeleteServicePorts(serviceName string) {
+	s.Lock()
+	defer s.Unlock()
+
+	delete(s.Services, serviceName)
+}
+
+func (s *ServiceMapping) GetServiceNextPort(serviceName string) (int, error) {
+	s.Lock()
+	defer s.Unlock()
+
+	serviceInfo, ok := s.Services[serviceName]
+	if !ok {
+		return 0, errors.New("no such service in services mapping file")
+	}
+
+	if len(serviceInfo.Ports.Items) == 0 {
+		return 0, errors.New("no ports info in services mapping file")
+	}
+
+	port, err := serviceInfo.Ports.Next()
+	if err != nil {
+		return 0, err
+	}
+	return port, nil
+}
+
+func (s *ServiceMapping) WatchFile(errChan chan<- error, logger *log.Logger) error {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	go func() {
+		defer watcher.Close()
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					errChan <- errors.New("error when wait on the events channel")
+					return
+				}
+				logger.Debug("received a file event", log.Any("eventName", event.Name), log.Any("eventOp", event.Op))
+
+				if event.Op&fsnotify.Write != fsnotify.Write {
+					continue
+				}
+
+				logger.Debug("load services mapping file again", log.Error(err))
+				err := s.load()
+				if err != nil {
+					logger.Warn("load services mapping file failed", log.Error(err))
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					errChan <- errors.New("error when wait on the events channel")
+					return
+				}
+				errChan <- errors.Trace(err)
+				return
+			}
+		}
+	}()
+
+	err = watcher.Add(ServiceMappingFile)
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	err = s.load()
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	return nil
 }
