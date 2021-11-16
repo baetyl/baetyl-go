62d836dbcfad047051b0dbd3f75083a4f62c099d
zhaoxin <zhaoxin26@baidu.com>
2021-11-16 14:42:37 +0800

    baetyl-1404 support containers (#287)

--- spec/v1/application.go
+++ spec/v1/application.go
@@ -60,6 +60,8 @@ type Service struct {
 	Devices []Device `json:"devices,omitempty" yaml:"devices,omitempty"`
 	// specifies the startup arguments of the service program, but does not include `arg[0]`
 	Args []string `json:"args,omitempty" yaml:"args,omitempty"`
+	// specifies the commands of the service program
+	Cmds []string `json:"cmds,omitempty" yaml:"cmds,omitempty"`
 	// specifies the environment variable of the service program
 	Env []Environment `json:"env,omitempty" yaml:"env,omitempty"`
 	// specifies resource limits for a single instance of the service,  only for Docker container mode
