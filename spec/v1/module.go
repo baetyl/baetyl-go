ce600a35fa5e4e3a531b84cbd7563a645719654b
zhaoxin26 <zhaoxin26@baidu.com>
2022-04-11 10:40:15 +0800

    baetyl-1642 add android struct

--- spec/v1/module.go
+++ spec/v1/module.go
@@ -1,12 +1,13 @@
 package v1
 
 const (
-	BaetylInit       = "baetyl-init"
-	BaetylCore       = "baetyl-core"
-	BaetylBroker     = "baetyl-broker"
-	BaetylFunction   = "baetyl-function"
-	BaetylRule       = "baetyl-rule"
-	BaetylAgent      = "baetyl-agent"
-	BaetylLog        = "baetyl-log"
-	BaetylGPUMetrics = "baetyl-accelerator-metrics"
+	BaetylInit        = "baetyl-init"
+	BaetylCore        = "baetyl-core"
+	BaetylCoreAndroid = "com.baidubce.baetyl.core"
+	BaetylBroker      = "baetyl-broker"
+	BaetylFunction    = "baetyl-function"
+	BaetylRule        = "baetyl-rule"
+	BaetylAgent       = "baetyl-agent"
+	BaetylLog         = "baetyl-log"
+	BaetylGPUMetrics  = "baetyl-accelerator-metrics"
 )
