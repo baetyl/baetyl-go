c32946534e910f8a503e35e4cd8a6157225037ce
wuao <18225560719@163.com>
2022-06-22 17:09:54 +0800

    baetyl-2013: Add mapping expression parsing

---
+++ dmcontext/expression_test.go
@@ -0,0 +1,83 @@
+package dmcontext
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestParseExpression(t *testing.T) {
+	args, err := ParseExpression("")
+	assert.NoError(t, err)
+	assert.Nil(t, args)
+
+	args, err = ParseExpression("&***)")
+	assert.Error(t, err)
+	assert.Nil(t, args)
+
+	args, err = ParseExpression("x1--x2")
+	assert.Error(t, err)
+	assert.Nil(t, args)
+
+	args, err = ParseExpression("4/(1+2+1*3*10)")
+	assert.NoError(t, err)
+	assert.NotNil(t, args)
+	assert.Equal(t, 0, len(args))
+
+	args, err = ParseExpression("x1")
+	assert.NoError(t, err)
+	assert.NotNil(t, args)
+	assert.Equal(t, 1, len(args))
+	assert.Equal(t, "x1", args[0])
+
+	args, err = ParseExpression("x4/(x1+x2+x1*x3*10)")
+	assert.NoError(t, err)
+	assert.NotNil(t, args)
+	assert.Equal(t, 5, len(args))
+	assert.Equal(t, "x1", args[1])
+}
+
+func TestExecExpression(t *testing.T) {
+	res, err := ExecExpression("", map[string]interface{}{}, "test")
+	assert.Error(t, err)
+	assert.Nil(t, res)
+
+	res, err = ExecExpression("", map[string]interface{}{}, MappingNone)
+	assert.NoError(t, err)
+	assert.Nil(t, res)
+
+	res, err = ExecExpression("", map[string]interface{}{}, MappingValue)
+	assert.Error(t, err)
+	assert.Nil(t, res)
+	res, err = ExecExpression("x1++x2", map[string]interface{}{}, MappingValue)
+	assert.Error(t, err)
+	assert.Nil(t, res)
+	res, err = ExecExpression("x1+x2", map[string]interface{}{}, MappingValue)
+	assert.Error(t, err)
+	assert.Nil(t, res)
+	res, err = ExecExpression("x1", map[string]interface{}{"x2": 1}, MappingValue)
+	assert.Error(t, err)
+	assert.Nil(t, res)
+	res, err = ExecExpression("x1", map[string]interface{}{"x1": 1}, MappingValue)
+	assert.NoError(t, err)
+	assert.Equal(t, 1, res)
+	res, err = ExecExpression("x1", map[string]interface{}{"x1": "1"}, MappingValue)
+	assert.NoError(t, err)
+	assert.Equal(t, "1", res)
+
+	res, err = ExecExpression("", map[string]interface{}{}, MappingCalculate)
+	assert.Error(t, err)
+	assert.Nil(t, res)
+	res, err = ExecExpression("x1++x2", map[string]interface{}{}, MappingCalculate)
+	assert.Error(t, err)
+	assert.Nil(t, res)
+	res, err = ExecExpression("x4/(x1+x2+x1*x3*10)", map[string]interface{}{"x1": 1}, MappingCalculate)
+	assert.Error(t, err)
+	assert.Nil(t, res)
+	res, err = ExecExpression("x4/(x1+x2+x1*x3*10)", map[string]interface{}{"x1": "asasd"}, MappingCalculate)
+	assert.Error(t, err)
+	assert.Nil(t, res)
+	args := map[string]interface{}{"x1": int16(1), "x2": float32(1.1), "x3": 0.99, "x4": int64(15)}
+	res, err = ExecExpression("x4/(x1+x2+x1*x3*10)", args, MappingCalculate)
+	assert.NoError(t, err)
+	assert.Equal(t, 1.25, res)
+}
