963bfa045f70784268da63fe1664d8ca68664e9f
miaodanyang <miaodanyang@baidu.com>
2023-05-12 13:54:32 +0800

    json add func

--- json/jsoniter.go
+++ json/jsoniter.go
@@ -1,6 +1,11 @@
 package json
 
-import jsoniter "github.com/json-iterator/go"
+import (
+	"io"
+	"strconv"
+
+	jsoniter "github.com/json-iterator/go"
+)
 
 func Marshal(v interface{}) ([]byte, error) {
 	json := jsoniter.ConfigCompatibleWithStandardLibrary
@@ -10,4 +15,29 @@ func Marshal(v interface{}) ([]byte, error) {
 func Unmarshal(data []byte, v interface{}) error {
 	json := jsoniter.ConfigCompatibleWithStandardLibrary
 	return json.Unmarshal(data, v)
+}
+
+func NewDecoder(reader io.Reader) *jsoniter.Decoder {
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	return json.NewDecoder(reader)
+}
+
+func NewEncoder(writer io.Writer) *jsoniter.Encoder {
+	json := jsoniter.ConfigCompatibleWithStandardLibrary
+	return json.NewEncoder(writer)
+}
+
+type Number string
+
+// String returns the literal text of the number.
+func (n Number) String() string { return string(n) }
+
+// Float64 returns the number as a float64.
+func (n Number) Float64() (float64, error) {
+	return strconv.ParseFloat(string(n), 64)
+}
+
+// Int64 returns the number as an int64.
+func (n Number) Int64() (int64, error) {
+	return strconv.ParseInt(string(n), 10, 64)
 }
