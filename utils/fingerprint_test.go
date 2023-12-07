a31f0703a93e97a14b849a6e8e2659c63af76dd6
miaodanyang <miaodanyang@baidu.com>
2023-12-07 14:13:14 +0800

    add fingerprint func abd add context done

--- utils/fingerprint_test.go
+++ utils/fingerprint_test.go
@@ -7,9 +7,8 @@ import (
 )
 
 func TestGetFingerprint(t *testing.T) {
-	id1, err := GetFingerprint("")
-	assert.NoError(t, err)
-	id2, err := GetFingerprint("")
-	assert.NoError(t, err)
+	id1, err1 := GetFingerprint("")
+	id2, err2 := GetFingerprint("")
+	assert.Equal(t, err1, err2)
 	assert.Equal(t, id1, id2)
 }
